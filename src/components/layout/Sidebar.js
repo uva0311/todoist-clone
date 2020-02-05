d1e9fa3f0b742602255be493b590ee07b913915e
Arthur Lam <lam.arthur0311@gmail.com>
2020-02-05 16:08:01 -0500

    created sidebar layout

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,5 +1,48 @@
 import React from 'react';
+import {
+  FaChevronDown,
+  FaInbox,
+  FaRegCalendarAlt,
+  FaRegCalendar
+} from 'react-icons/fax';
 
 export const Sidebar = () => {
-  return <p>I am a sidebar</p>;
+  return (
+    <div className='sidebar' data-testiod='sidebar'>
+      <ul className='sidebar__generic'>
+        <li>
+          <span>
+            <FaInbox />
+          </span>
+          <span>Inbox</span>
+        </li>
+        <li>
+          <span>
+            <FaRegCalendar />
+          </span>
+          <span>Today</span>
+        </li>
+        <li>
+          <span>
+            <FaRegCalendarAlt />
+          </span>
+          <span>Next 7 days</span>
+        </li>
+        <li>
+          <span>
+            <FaRegCalendar />
+          </span>
+          <span>Today</span>
+        </li>
+      </ul>
+      <div className='sidebar__middle'>
+        <span>
+          <FaChevronDown />
+        </span>
+        <h2>Projects</h2>
+      </div>
+      <ul className='sidebar__projects'>Projects will be here!</ul>
+      Add Projects Component Here!!
+    </div>
+  );
 };
