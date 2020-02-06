6a09f486cd509e94ce31f17493a1a9d748d45568
Arthur Lam <lam.arthur0311@gmail.com>
2020-02-05 19:50:54 -0500

    included attributes on html element

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -10,29 +10,23 @@ export const Sidebar = () => {
   return (
     <div className='sidebar' data-testiod='sidebar'>
       <ul className='sidebar__generic'>
-        <li>
+        <li data-testid='inbox' className='inbox'>
           <span>
             <FaInbox />
           </span>
           <span>Inbox</span>
         </li>
-        <li>
+        <li data-testid='today' className='today'>
           <span>
             <FaRegCalendar />
           </span>
           <span>Today</span>
         </li>
-        <li>
+        <li data-testid='next_7' className='next_7'>
           <span>
             <FaRegCalendarAlt />
           </span>
           <span>Next 7 days</span>
-        </li>
-        <li>
-          <span>
-            <FaRegCalendar />
-          </span>
-          <span>Today</span>
         </li>
       </ul>
       <div className='sidebar__middle'>
