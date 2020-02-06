c6feee09cea0786c02fca25461e64b93a3ddb97a
Arthur Lam <lam.arthur0311@gmail.com>
2020-02-06 13:31:45 -0500

    implemented helper functions to get project items

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,5 +1,13 @@
 import { collatedTasks } from '../constants';
 
+export const getTitle = (projects, projectId) => {
+  return projects.find(project => project.projectId === projectId);
+};
+
+export const getCollatedTitle = (projects, key) => {
+  return projects.find(project => project.key === key);
+};
+
 export const collatedTasksExist = selectedProject => {
   return collatedTasks.find(task => task.key === selectedProject);
 };
