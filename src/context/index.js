5bb28e2b5fe05d50cc843aff5ae50022db9441b0
Arthur Lam <lam.arthur0311@gmail.com>
2020-02-06 11:52:38 -0500

    create contexts to pass project items on sidebar component

---
+++ src/context/index.js
@@ -0,0 +1,20 @@
+import {
+  ProjectsContext,
+  ProjectsProvider,
+  useProjectValue
+} from './projects-context';
+
+import {
+  SelectedProjectContext,
+  SelectedProjectProvider,
+  useSelectedProjectValue
+} from './selected-projects-context';
+
+export {
+  ProjectsContext,
+  ProjectsProvider,
+  useProjectValue,
+  SelectedProjectContext,
+  SelectedProjectProvider,
+  useSelectedProjectValue
+};
