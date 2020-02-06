3ef0f4fa978ee33d87862e0d415c748099e53254
Arthur Lam <lam.arthur0311@gmail.com>
2020-02-06 11:53:32 -0500

    import context for displaying project items stored

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   FaChevronDown,
   FaInbox,
   FaRegCalendarAlt,
   FaRegCalendar
 } from 'react-icons/fa';
+import { useSelectedProjectValue } from '../../context';
 
 export const Sidebar = () => {
+  const { setSelectedProject } = useSelectedProjectValue;
+  const [active, setActive] = useState('inbox');
+  const [showProjects, setShowProjects] = useState(true);
+
   return (
     <div className='sidebar' data-testiod='sidebar'>
       <ul className='sidebar__generic'>
