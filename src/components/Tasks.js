337764e64b77625b1e020431aee551bda0a99d44
Arthur Lam <lam.arthur0311@gmail.com>
2020-02-06 13:32:26 -0500

    imported helper functions to get tasks

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import { Checkbox } from './Checkbox';
 import { useTasks } from '../hooks';
+import { collatedTasks } from '../constants';
+import { getTitle, getCollatedTitle, collatedTasksExist } from '../helpers';
+import { useSelectedProjectValue, useProjectValue } from '../context';
 
 export const Tasks = () => {
-  const { tasks } = useTasks('1');
+  const { selectedProject } = useSelectedProjectValue();
+  const { projects } = useProjectValue();
+  const { tasks } = useTasks(selectedProject);
 
   const projectName = '';
+
+  if (projectss && selectedProject && !collatedTasksExist(selectedProject)) {
+    projectName = getTitle(projects, selectedProject).name;
+  }
+
+  if (collatedTasks(selectedProject) && selectedProject) {
+    projectName = getCollatedTitle(collatedTasks, selectedProject).name;
+  }
+
+  useEffect(() => {
+    document.title = `${projectName}: Todoist`;
+  });
 
   return (
     <div className='tasks' data-testid='tasks'>
