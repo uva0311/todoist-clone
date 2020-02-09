ce704ee180ce418deb040222d10e6e517bc864e8
Arthur Lam <lam.arthur0311@gmail.com>
2020-02-09 16:15:38 -0500

    clean configuration

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,17 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 
+/* insert your own configuration to get the app work if you are downloading the project for further modification*/
+
 const firebaseConfig = firebase.initializeApp({
-  apiKey: 'AIzaSyAcruAstQ9RKEa7p39dOX1WR5P9-SNyT9g',
-  authDomain: 'todoist-db-97cd6.firebaseapp.com',
-  databaseURL: 'https://todoist-db-97cd6.firebaseio.com',
-  projectId: 'todoist-db-97cd6',
-  storageBucket: 'todoist-db-97cd6.appspot.com',
-  messagingSenderId: '23415951096',
-  appId: '1:23415951096:web:16350a89d983e80fe9b50d',
-  measurementId: 'G-R38YJXH9Z2'
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+  measurementId: ''
 });
 
 export { firebaseConfig as firebase };
